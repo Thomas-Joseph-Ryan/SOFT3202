c94c0081560b8a84d7642e7f25cf2352c35b5699
tommy <tjryan13579@gmail.com>
2023-05-17 11:37:52 +1000

    Figuring out Order stuff

--- Assignment2/BrawndoCodebase/src/main/java/au/edu/sydney/brawndo/erp/spfea/ordering/OneOffOrder.java
+++ Assignment2/BrawndoCodebase/src/main/java/au/edu/sydney/brawndo/erp/spfea/ordering/OneOffOrder.java
@@ -4,66 +4,177 @@ import au.edu.sydney.brawndo.erp.ordering.Order;
 import au.edu.sydney.brawndo.erp.ordering.Product;
 
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
+
+@SuppressWarnings("Duplicates")
 public class OneOffOrder implements Order {
-    @Override
-    public int getOrderID() {
-        return 0;
-    }
+    private Map<Product, Integer> products = new HashMap<>();
+    private int id;
+    private LocalDateTime date;
+    private double discountRate;
+    private int customerID;
+    private boolean finalised = false;
 
-    @Override
-    public double getTotalCost() {
-        return 0;
+    public OneOffOrder(int id, LocalDateTime date, double discountRate, int customerID) {
+        this.id = id;
+        this.date = date;
+        this.discountRate = discountRate;
+        this.customerID = customerID;
     }
 
     @Override
     public LocalDateTime getOrderDate() {
-        return null;
+        return date;
     }
 
     @Override
     public void setProduct(Product product, int qty) {
+        if (finalised) throw new IllegalStateException("Order was already finalised.");
 
+        // We can't rely on like products having the same object identity since they get
+        // rebuilt over the network, so we had to check for presence and same values
+
+        for (Product contained: products.keySet()) {
+            if (contained.equals(product)) {
+                product = contained;
+                break;
+            }
+        }
+
+        products.put(product, qty);
     }
 
     @Override
     public Set<Product> getAllProducts() {
-        return null;
+        return products.keySet();
     }
 
     @Override
     public int getProductQty(Product product) {
-        return 0;
-    }
+        // We can't rely on like products having the same object identity since they get
+        // rebuilt over the network, so we had to check for presence and same values
 
-    @Override
-    public String generateInvoiceData() {
-        return null;
+        for (Product contained: products.keySet()) {
+            if (contained.equals(product)) {
+                product = contained;
+                break;
+            }
+        }
+
+        Integer result = products.get(product);
+        return null == result ? 0 : result;
     }
 
     @Override
     public int getCustomer() {
-        return 0;
-    }
-
-    @Override
-    public void finalise() {
-
+        return customerID;
     }
 
     @Override
     public Order copy() {
-        return null;
+        Order copy = new Order66(id, date, discountRate, customerID);
+        for (Product product: products.keySet()) {
+            copy.setProduct(product, products.get(product));
+        }
+
+        return copy;
+    }
+
+    @Override
+    public String generateInvoiceData() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Thank you for your Brawndo© order!\n");
+        sb.append("Your order comes to: $");
+        sb.append(String.format("%,.2f", getTotalCost()));
+        sb.append("\nPlease see below for details:\n");
+        List<Product> keyList = new ArrayList<>(products.keySet());
+        keyList.sort(Comparator.comparing(Product::getProductName).thenComparing(Product::getCost));
+
+        for (Product product: keyList) {
+            sb.append("\tProduct name: ");
+            sb.append(product.getProductName());
+            sb.append("\tQty: ");
+            sb.append(products.get(product));
+            sb.append("\tCost per unit: ");
+            sb.append(String.format("$%,.2f", product.getCost()));
+            sb.append("\tSubtotal: ");
+            sb.append(String.format("$%,.2f\n", product.getCost() * products.get(product)));
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public double getTotalCost() {
+        double cost = 0.0;
+        for (Product product: products.keySet()) {
+            cost +=  products.get(product) * product.getCost() * discountRate;
+        }
+        return cost;
+    }
+
+    protected double getDiscountRate() {
+        return this.discountRate;
+    }
+
+    protected Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+    @Override
+    public int getOrderID() {
+        return id;
+    }
+
+    @Override
+    public void finalise() {
+        this.finalised = true;
     }
 
     @Override
     public String shortDesc() {
-        return null;
+        return String.format("ID:%s $%,.2f", id, getTotalCost());
     }
 
     @Override
     public String longDesc() {
-        return null;
+        double fullCost = 0.0;
+        double discountedCost = getTotalCost();
+        StringBuilder productSB = new StringBuilder();
+
+        List<Product> keyList = new ArrayList<>(products.keySet());
+        keyList.sort(Comparator.comparing(Product::getProductName).thenComparing(Product::getCost));
+
+        for (Product product: keyList) {
+            double subtotal = product.getCost() * products.get(product);
+            fullCost += subtotal;
+
+            productSB.append(String.format("\tProduct name: %s\tQty: %d\tUnit cost: $%,.2f\tSubtotal: $%,.2f\n",
+                    product.getProductName(),
+                    products.get(product),
+                    product.getCost(),
+                    subtotal));
+        }
+
+        return String.format(finalised ? "" : "*NOT FINALISED*\n" +
+                        "Order details (id #%d)\n" +
+                        "Date: %s\n" +
+                        "Products:\n" +
+                        "%s" +
+                        "\tDiscount: -$%,.2f\n" +
+                        "Total cost: $%,.2f\n",
+                id,
+                date.format(DateTimeFormatter.ISO_LOCAL_DATE),
+                productSB.toString(),
+                fullCost - discountedCost,
+                discountedCost
+        );
+    }
+
+    protected boolean isFinalised() {
+        return finalised;
     }
 }
--- Assignment2/BrawndoCodebase/src/main/java/au/edu/sydney/brawndo/erp/spfea/ordering/Strategies/InvoiceType/InvoiceData.java
+++ Assignment2/BrawndoCodebase/src/main/java/au/edu/sydney/brawndo/erp/spfea/ordering/Strategies/InvoiceType/InvoiceData.java
@@ -2,15 +2,16 @@ package au.edu.sydney.brawndo.erp.spfea.ordering.Strategies.InvoiceType;
 
 import au.edu.sydney.brawndo.erp.ordering.Product;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class InvoiceData {
-    private double totalCost;
-    private Map<Product, Integer> products;
+    private final double totalCost;
+    private final Map<Product, Integer> products;
 
-    public InvoiceData(double totalCost, Map<Product, Integer> products) {
-        this.totalCost = totalCost;
-        this.products = products;
+    private InvoiceData(Builder builder) {
+        this.totalCost = builder.totalCost;
+        this.products = builder.products;
     }
 
     public double getTotalCost() {
@@ -19,5 +20,24 @@ public class InvoiceData {
 
     public Map<Product, Integer> getProducts() {
         return products;
+    }
+
+    public static class Builder {
+        private double totalCost = 0.0;
+        private Map<Product, Integer> products = new HashMap<>();
+
+        public Builder totalCost(double totalCost) {
+            this.totalCost = totalCost;
+            return this;
+        }
+
+        public Builder products(Map<Product, Integer> products) {
+            this.products = products;
+            return this;
+        }
+
+        public InvoiceData build() {
+            return new InvoiceData(this);
+        }
     }
 }
