537ffe66e6451c8a4cff369796c4ad035a620ac3
Thomas-Joseph-Ryan <tjryan13579@gmail.com>
2023-05-20 16:32:36 +1000

    Up to task 7, need to figure out how to save to repo

--- Tasks/Task4/src/main/java/au/sydney/soft3202/task1/ShoppingController.java
+++ Tasks/Task4/src/main/java/au/sydney/soft3202/task1/ShoppingController.java
@@ -1,11 +1,7 @@
 package au.sydney.soft3202.task1;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.RepresentationModel;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Optional;
 
 class Item extends RepresentationModel<Item>  {
     public Integer id;
@@ -52,4 +47,103 @@ public class ShoppingController {
         }
         return bs;
     }
+
+    @GetMapping("users")
+    public List<String> getUsers() {
+        List<ShoppingBasket> baskets = shoppingBasket.findAll();
+        List<String> users = new LinkedList<>();
+        for (ShoppingBasket basket : baskets) {
+            users.add(basket.getUser());
+        }
+
+        return users;
+    }
+
+    public record ItemCost(String name, String cost) {}
+
+    @PostMapping("costs")
+    public ResponseEntity<Void> addItemCost(@RequestBody ItemCost itemCost) {
+
+        costs.put(itemCost.name, Double.valueOf(itemCost.cost));
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("costs/{name}")
+    public ResponseEntity<Void> updateItemCost(@PathVariable String name, @RequestBody ItemCost itemCost) {
+        if (costs.containsKey(name)) {
+            costs.replace(name, Double.valueOf(itemCost.cost));
+        }
+
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("costs")
+    public Map<String, Double> getCosts() {
+        List<ShoppingBasket> baskets = shoppingBasket.findAll();
+        for (ShoppingBasket basket : baskets) {
+            if (!costs.containsKey(basket.getName())) {
+                costs.put(basket.getName(), 0.00);
+            }
+        }
+
+        return costs;
+    }
+
+    record ItemAndCount(String name, Integer count) {
+    }
+
+    @PostMapping("users/{username}/add")
+    public ResponseEntity<Void> addItemToUserCart(@PathVariable String username, @RequestBody ItemAndCount itemAndCount) {
+        if (costs.containsKey(itemAndCount.name)) {
+            Boolean added = false;
+            List<ShoppingBasket> baskets = shoppingBasket.findAll();
+            for (ShoppingBasket basket : baskets) {
+                if (basket.getUser().equals(username) && basket.getName().equals(itemAndCount.name)) {
+                    basket.setCount(basket.getCount() + itemAndCount.count);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                ShoppingBasket shoppingBasket = new ShoppingBasket();
+                shoppingBasket.setUser(username);
+                shoppingBasket.setCount(itemAndCount.count);
+                shoppingBasket.setName(itemAndCount.name);
+//                Save basket?
+            }
+        }
+    }
+
+    @GetMapping("users/{username}")
+    public List<ItemAndCount> getItemsOfUser(@PathVariable String username) {
+        List<ShoppingBasket> baskets = shoppingBasket.findAll();
+
+        List<ItemAndCount> items = new LinkedList<>();
+        for (ShoppingBasket basket : baskets) {
+            if (basket.getUser().equals(username)) {
+                items.add(new ItemAndCount(basket.getName(), basket.getCount()));
+            }
+        }
+
+        return items;
+    }
+
+    @GetMapping("users/{username}/total")
+    public Double getCostForUser(@PathVariable String username) {
+        List<ShoppingBasket> baskets = shoppingBasket.findAll();
+        Double cost = 0.0;
+        for (ShoppingBasket basket : baskets) {
+            if (!costs.containsKey(basket.getName())) {
+                costs.put(basket.getName(), 0.00);
+            }
+
+            if (basket.getUser().equals(username)) {
+                cost += costs.get(basket.getName());
+            }
+        }
+        return cost;
+    }
+
+
+
 }
